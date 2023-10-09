7b9a69ee8237d5506395e5970b9ac8a54721f75e
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-09 06:02:41 +0530

    wrote sig up logic for  our app and passed some data onto our provider

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,15 +1,29 @@
-import {createContext,useContext,useEffect,useState} from 'react'
+import { createContext, useContext, useEffect, useState } from "react";
+
+import { auth } from "../firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 const AuthContext = createContext();
 
-export function AuthContextProvider({children}){
-    return(
-        <AuthContext.Provider>
-            {children}
-        </AuthContext.Provider>
-    )
+export function AuthContextProvider({ children }) {
+  const [user, setUser] = useState({});
+
+  function signUp(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  return (
+    <AuthContext.Provider value={{ signUp, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
-export function UserAuth(){
-    return useContext(AuthContext);
+export function UserAuth() {
+  return useContext(AuthContext);
 }
