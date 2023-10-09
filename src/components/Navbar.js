62285c1ef041e5b5003039752859984d62b8992e
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-09 07:25:18 +0530

    added logout feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,48 @@
-import {Link} from 'react-router-dom'
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 const Navbar = () => {
+  const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
+  // console.log(user);
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div className='flex items-center justify-between py-4 z-[100] absolute w-full'>
-        <Link to="/">
-        <h1 className='text-red-600 text-4xl font-semi cursor-pointer'>NETFLIX</h1>
-        </Link>
+    <div className="flex items-center justify-between py-4 z-[100] absolute w-full">
+      <Link to="/">
+        <h1 className="text-red-600 text-4xl font-semi cursor-pointer">
+          NETFLIX
+        </h1>
+      </Link>
+      {user?.email ? (
+        <div>
+          <Link to="/account">
+            <button className="text-white pr-4">Account</button>
+          </Link>
+          <button onClick={handleLogout} className="text-white bg-red-600 px-6 py-2 rounded cursor-pointer">
+            Log Out
+          </button>
+        </div>
+      ) : (
         <div>
           <Link to="/login">
-            <button className='text-white pr-4'>Sign In</button>
-            </Link>
+            <button className="text-white pr-4">Log In</button>
+          </Link>
           <Link to="/signup">
-            <button className='text-white bg-red-600 px-6 py-2 rounded cursor-pointer'>Sign Up</button>
-            </Link>
+            <button className="text-white bg-red-600 px-6 py-2 rounded cursor-pointer">
+              Sign Up
+            </button>
+          </Link>
         </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
