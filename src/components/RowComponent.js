70ba4c969d34a122e60018f73cd3c5f74b655a82
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-09 05:35:47 +0530

    fixed the scroll thing

--- src/components/RowComponent.js
+++ src/components/RowComponent.js
@@ -3,19 +3,19 @@ import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
-const RowComponent = ({ title, fetchURL }) => {
+const RowComponent = ({ title, fetchURL, rowId}) => {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     axios.get(fetchURL).then((res) => setMovies(res.data.results));
   }, [fetchURL]);
 
  const slideLeft=()=>{
-    var slider=document.getElementById('slider');
+    var slider=document.getElementById('slider'+rowId);
     slider.scrollLeft=slider.scrollLeft-500;
  }
 
  const slideRight=()=>{
-    var slider=document.getElementById('slider');
+    var slider=document.getElementById('slider'+rowId);
     slider.scrollLeft=slider.scrollLeft+500;
  }
 
@@ -26,7 +26,7 @@ const RowComponent = ({ title, fetchURL }) => {
       <div className="relative flex items-center group">
         <FaChevronLeft className="bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block left-0" size={40} onClick={slideLeft}/>
         <div
-          id={"slider"}
+          id={"slider"+rowId}
           className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
         >
           {movies.map((movie, id) => (
