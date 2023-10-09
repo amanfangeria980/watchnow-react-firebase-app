e7c6040e18147e3d78365d40d6a41372503fa679
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-09 07:08:16 +0530

    sign up working

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,5 +1,23 @@
 import {Link} from 'react-router-dom'
+import {UserAuth} from "../context/AuthContext"
+import { useState } from 'react';
+
 const SignUp = () => {
+  const [email,setEmail]=useState('');
+  const [password,setPassword]=useState('');
+  const {user,signUp}=UserAuth();
+
+  const handleSubmit=async (e)=>{
+    e.preventDefault();
+    try{
+      await signUp(email,password);
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+
+
   return (
     <>
     <div className='w-full h-screen'>
@@ -9,9 +27,9 @@ const SignUp = () => {
         <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
           <div className='max-w-[320px] mx-auto py-16'>
             <h1 className='text-3xl font-bold'>Sign Up</h1>
-            <form className='w-full flex flex-col py-4'>
-              <input className="p-3 my-2 bg-gray-700 rounded " type="email" placeholder='Email' autoComplete='email'/>
-              <input className="p-3 my-2 bg-gray-700 rounded " type="password" placeholder='Password' autoComplete='current-password'/>
+            <form className='w-full flex flex-col py-4' onSubmit={handleSubmit}>
+              <input className="p-3 my-2 bg-gray-700 rounded " type="email" placeholder='Email' autoComplete='email' onChange={(e)=>setEmail(e.target.value)}/>
+              <input className="p-3 my-2 bg-gray-700 rounded " type="password" placeholder='Password' autoComplete='current-password' onChange={(e)=>setPassword(e.target.value)}/>
               <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign Up</button>
               <div className='flex justify-between items-center text-sm text-gray-600'>
                 <p className='mr-2'>
