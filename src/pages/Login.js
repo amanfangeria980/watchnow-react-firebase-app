32148ad72335872bbe88e8fb0802e2b58951e173
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-09 07:46:45 +0530

    login feature working

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,24 @@
-import {Link} from 'react-router-dom'
+import {Link,useNavigate} from 'react-router-dom'
+import {UserAuth} from "../context/AuthContext"
+import { useState } from 'react';
 const Login = () => {
+  const [email,setEmail]=useState('');
+  const [password,setPassword]=useState('');
+  const [error,setError]=useState('');
+  const {user,logIn}=UserAuth();
+  const navigate=useNavigate();
+  const handleSubmit=async (e)=>{
+    e.preventDefault();
+    setError('');
+    try{
+      await logIn(email,password);
+      navigate("/")
+    }
+    catch(err){
+      console.log(err);
+      setError(err.message);
+    }
+  }
   return (
     <>
     <div className='w-full h-screen'>
@@ -9,10 +28,11 @@ const Login = () => {
         <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
           <div className='max-w-[320px] mx-auto py-16'>
             <h1 className='text-3xl font-bold'>Log In</h1>
-            <form className='w-full flex flex-col py-4'>
-              <input className="p-3 my-2 bg-gray-700 rounded " type="email" placeholder='Email' autoComplete='email'/>
-              <input className="p-3 my-2 bg-gray-700 rounded " type="password" placeholder='Password' autoComplete='current-password'/>
-              <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign Up</button>
+            {error ? <p className='p-3 bg-red-400 my-2'>{error}</p>:null}
+            <form className='w-full flex flex-col py-4' onSubmit={handleSubmit}>
+              <input className="p-3 my-2 bg-gray-700 rounded " type="email" placeholder='Email' autoComplete='email' onChange={(e)=>setEmail(e.target.value)}/>
+              <input className="p-3 my-2 bg-gray-700 rounded " type="password" placeholder='Password' autoComplete='current-password' onChange={(e)=>setPassword(e.target.value)}/>
+              <button className='bg-red-600 py-3 my-6 rounded font-bold'>Log In</button>
               <div className='flex justify-between items-center text-sm text-gray-600'>
                 <p className='mr-2'>
                   <input type="checkbox" />Remember me
