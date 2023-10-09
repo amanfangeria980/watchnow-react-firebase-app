acdbec92105febad428692a54e1442ad91b6f4f8
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-09 06:32:17 +0530

    linked our home page, login & signup page to our routes and did the routing for all the pages in our app.js file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,17 @@
-import React from 'react'
-
+import {Link} from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-4 z-[100] absolute w-full'>
+        <Link to="/">
         <h1 className='text-red-600 text-4xl font-semi cursor-pointer'>NETFLIX</h1>
+        </Link>
         <div>
+          <Link to="/login">
             <button className='text-white pr-4'>Sign In</button>
+            </Link>
+          <Link to="/singup">
             <button className='text-white bg-red-600 px-6 py-2 rounded cursor-pointer'>Sign Up</button>
+            </Link>
         </div>
     </div>
   )
