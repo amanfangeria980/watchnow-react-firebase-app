fe348c7a46cefcf515a726f5aad943e2e97dc22c
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-09 06:27:10 +0530

    bug-forgot to add env file

--- src/firebase.js
+++ src/firebase.js
@@ -6,6 +6,7 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
