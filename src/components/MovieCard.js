c54320cc8d79d1d3cef352e348ff1ff72f2dde57
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-09 18:31:36 +0530

    adding and storing to firebase working

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,6 +6,28 @@ import { arrayUnion,doc,updateDoc } from 'firebase/firestore';
 
 const MovieCard=({movie})=>{
     const [like,setLike]=useState(false);
+    const [saved,setSaved]=useState(false);
+    const {user}=UserAuth();
+    // grabbing the user email from db
+    const movieId=doc(db,'users',`${user?.email}`);
+
+    const savedShow=async()=>{
+      if(user?.email){
+        setLike(!like);
+        setSaved(true);
+        await updateDoc(movieId,{
+          savedShows:arrayUnion({
+            id: movie.id,
+            title: movie.title,
+            img: movie.backdrop_path
+          })
+        })
+      }
+      else{
+        alert("Please login to save a movie");  
+      }
+    }
+
     return(
         <>
               <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2 text-white">
@@ -16,7 +38,7 @@ const MovieCard=({movie})=>{
                 />
                 <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 text-white hover:opacity-100">
                     <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">{movie?.title}</p>
-                    <p className="absolute top-4 left-4 text-gray-300">
+                    <p className="absolute top-4 left-4 text-gray-300" onClick={savedShow}>
                         {like? <FaHeart/>: <FaRegHeart/>}
                     </p>
                 </div>
