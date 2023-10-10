906d35f5711e068e7fd76fd289323ca1bf80ff65
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-10 18:24:04 +0530

    styled login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -25,21 +25,21 @@ const Login = () => {
       <img className="hidden sm:block absolute w-full h-full object-cover" src="https://previews.123rf.com/images/olegdudko/olegdudko1907/olegdudko190705921/128026353-movie-clapper-and-film-reel-on-a-wooden-background.jpg" alt="background auth" />
       <div className='absolute bg-black/60 top-0 left-0 w-full h-screen '></div>
       <div className='fixed w-full px-4 py-24 z-50'>
-        <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
+        <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-green-600'>
           <div className='max-w-[320px] mx-auto py-16'>
             <h1 className='text-3xl font-bold'>Log In</h1>
-            {error ? <p className='p-3 bg-red-400 my-2'>{error}</p>:null}
+            {error ? <p className='p-3 bg-lime-400 my-2 text-black'>{error}</p>:null}
             <form className='w-full flex flex-col py-4' onSubmit={handleSubmit}>
               <input className="p-3 my-2 bg-gray-700 rounded " type="email" placeholder='Email' autoComplete='email' onChange={(e)=>setEmail(e.target.value)}/>
               <input className="p-3 my-2 bg-gray-700 rounded " type="password" placeholder='Password' autoComplete='current-password' onChange={(e)=>setPassword(e.target.value)}/>
-              <button className='bg-red-600 py-3 my-6 rounded font-bold'>Log In</button>
+              <button className='bg-lime-400 py-3 my-6 rounded text-black font-bold'>Log In</button>
               <div className='flex justify-between items-center text-sm text-gray-600'>
                 <p className='mr-2'>
                   <input type="checkbox" />Remember me
                 </p>
                 <p>Need Help?</p>
               </div>
-              <p className='py-8'><span className='text-gray-600'>New to NetFlix? </span> <Link to='/signup'>Sign Up</Link></p>
+              <p className='py-8'><span className='text-gray-600'>Sign Up Now! </span> <Link to='/signup'>Sign Up</Link></p>
             </form>
           </div>
         </div>
