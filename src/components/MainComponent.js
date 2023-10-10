73e5c6294c06b5e11e938f7c1823d8d11800862a
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-10 18:07:09 +0530

    styled main component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -25,16 +25,16 @@ const Main = () => {
 
   return (
     <>
-    <div className='w-full h-[550px] text-white'>
+    <div className='w-full h-[550px] text-gray-300'>
         <div className='w-full h-full'>
-            <div className='absolute w-full h-[550px] bg-gradient-to-r from-black'></div>
+            <div className='absolute w-full h-[550px] bg-gradient-to-l from-black'></div>
             <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
             <div>
                 <div className='absolute w-full top-[20%] p-4 md:p-8'>
                     <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
                     <div className='my-4'>
-                        <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>Play</button>
-                        <button className='border text-white border-gray-300 py-2 px-5 ml-4'>Watch Later</button>
+                        <button className='border bg-green-300 text-black border-gray-300 py-2 px-5'>Play</button>
+                        <button className='border text-white border-green-300 py-2 px-5 ml-4'>Watch Later</button>
                     </div>
                     <p className='text-gray-400 text-sm'>Released: {movie?.release_date}</p>
                     <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%]'>{truncateString(movie?.overview,150)}</p>
